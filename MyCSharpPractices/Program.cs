aaa974e1330993fe130106306d035cf0af9e104a
Caleb Adekeye <cadekeye@thomasmurray.com>
2019-08-13 11:06:21 +0100

    second commit

--- MyCSharpPractices/Program.cs
+++ MyCSharpPractices/Program.cs
@@ -135,4 +135,46 @@ namespace MyCSharpPractices
 
 
     }
+
+    public class Song
+    {
+        private string name;
+        public Song NextSong { get; set; }
+
+        public Song(string name)
+        {
+            this.name = name;
+        }
+
+        public bool IsRepeatingPlaylist()
+        {
+            Song slowStep = this;
+            Song fastStep = this;
+
+
+            while (fastStep?.NextSong != null)
+            {
+                slowStep = slowStep.NextSong;
+                fastStep = fastStep.NextSong.NextSong;
+
+                if (slowStep == fastStep)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static void TestRepeating()
+        {
+            Song first = new Song("Hello");
+            Song second = new Song("Eye of the tiger");
+
+            first.NextSong = second;
+            second.NextSong = first;
+
+            Console.WriteLine(first.IsRepeatingPlaylist());
+        }
+    }
 }
